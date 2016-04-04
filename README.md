b6cf9c3a33b9608dbab3c72debc4478992a74cdd
lxynox from github <lxynox@gmail.com>
2016-04-04 00:46:39 -0400

    WIP: setting up jspm

---
+++ README.md
@@ -0,0 +1,2 @@
+# ES6-project
+Setup new project using ES6 features. Babel + jspm/npm + node + gulp + grunt + karma + mocha(test)
---
+++ src/example.js
@@ -0,0 +1,6 @@
+const fragment = '<example>
+                    <h1>jspm, SystemJS, Babel, Seed</h1>
+                  </example>';
+export default(mainElement) => {
+  mainElement.insertAdjacentHTML('afterbegin', fragment);
+};
